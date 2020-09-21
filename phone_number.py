0d3c77f1526c264413fb83918d90720e1af6fa22
SGiuri <s.giuri@issm.it>
2020-09-21 22:38:10 +0200

    Done

--- phone_number.py
+++ phone_number.py
@@ -1,45 +1,36 @@
-"""
-The format is usually represented as
-
-(NXX)-NXX-XXXX
-where N is any digit from 2 through 9 and X is any digit from 0 through 9.
-
-Your task is to clean up differently formatted telephone numbers by removing punctuation and the country code (1) if present.
-
-For example, the inputs
-
-+1 (613)-995-0253
-613-995-0253
-1 613 995 0253
-613.995.0253
-should all produce the output
-
-6139950253
-"""
-
 import re
 
 class PhoneNumber:
+
     def __init__(self, number):
-        # removing and cleaning from not numbers
+        # removing and cleaning everything that in not a a number
         clean_number = re.sub(r"[^0-9]", "", number)
 
-        # checking if there are the initial 1:
+        # checking if there are the initial 1, if so, removing it:
         clean_number = re.sub(r"^1?", "", clean_number)
+
+        # the number must now be in the length of 10
         if len(clean_number)!= 10:
             raise ValueError("Not Valid Number")
+
         #checking if it is a valid number#
         number_pattern = re.compile("([2-9]{1}[0-9]{2}[2-9]{1}[0-9]{2}[0-9]{4})")
         matches = re.search(number_pattern,clean_number)
 
+        # if valid i found something matching the pattern
         if matches:
             self.number = matches.group(1)
         else:
             raise ValueError("Not Valid Number")
 
+        # isolatging the area code
         self.area_code = self.number[:3]
 
     def pretty(self):
+        """
+        A method that return the Phonenumber like this:
+        (266)-266-1234
+        """
 
         return f"({self.area_code})-{self.number[3:6]}-{self.number[6:]}"
 
