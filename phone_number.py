177067dd179840047446550ac3c5e1ddd1b06b30
SGiuri <s.giuri@issm.it>
2020-09-21 15:37:07 +0200

    Manca documntazione

--- phone_number.py
+++ phone_number.py
@@ -21,7 +21,33 @@ import re
 
 class PhoneNumber:
     def __init__(self, number):
-        self.number = number
-        pattern = re.compile(r"(/+)?1?[2-9][0-9]{2}")
+        # removing and cleaning from not numbers
+        clean_number = re.sub(r"[^0-9]", "", number)
 
-        pass
+        # checking if there are the initial 1:
+        clean_number = re.sub(r"^1?", "", clean_number)
+        if len(clean_number)!= 10:
+            raise ValueError("Not Valid Number")
+        #checking if it is a valid number#
+        number_pattern = re.compile("([2-9]{1}[0-9]{2}[2-9]{1}[0-9]{2}[0-9]{4})")
+        matches = re.search(number_pattern,clean_number)
+
+        if matches:
+            self.number = matches.group(1)
+        else:
+            raise ValueError("Not Valid Number")
+
+        self.area_code = self.number[:3]
+
+    def pretty(self):
+
+        return f"({self.area_code})-{self.number[3:6]}-{self.number[6:]}"
+
+
+
+
+
+
+
+
+number = PhoneNumber("+1 (223) 456-7890").number
